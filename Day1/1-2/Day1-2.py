3e8dc062b004c8507b5c5a629b35905f218a3745
gautier astoul <gautier.astoul@gmail.com>
2020-02-03 22:59:42 +0100

    new file to easily use python interpreter on notepad ++

--- Day1/1-2/Day1-2.py
+++ Day1/1-2/Day1-2.py
@@ -20,10 +20,10 @@ def sum_of_element(list):
 
 def zero_tracking(list_element):
 	list_element = calcul(list_element)
-		while list_element > 0 :
-			list_element = calcul(list_element)
-			result += list_element
-			return result
+	while list_element > 0 :
+		list_element = calcul(list_element)
+		result += list_element
+		return result
 
 ## I open the file which containt some integers in strings
 ## I put them on a list, I transform them to be ready for calculation
@@ -32,8 +32,8 @@ with open("Day1.txt") as sum_of_fuel:
     sum_of_fuel = [x.rstrip("\n") for x in sum_of_fuel]
     sum_of_fuel = [int(x) for x in sum_of_fuel]
 ## For each item on a list I do a simple calculation
-sum_of_fuel = [simple_calcul(x) for x in sum_of_fuel]
 print(f"After calculation each elements of fuel are:\n{sum_of_fuel}\n")
+sum_of_fuel = [simple_calcul(x) for x in sum_of_fuel]
 
 ## Need to add each lines on the list 
 result_sum_of_fuel = sum_of_element(sum_of_fuel)
@@ -42,7 +42,9 @@ print(f"The total of each element of fuel is:\n{result_sum_of_fuel}\n")
 ##
 intermediate_fuel_calcul = sum_of_fuel.copy()
 print(f"The copy is :\n{intermediate_fuel_calcul}\n")
-
+for i in range (len(intermediate_fuel_calcul)):
+	intermediate_fuel_calcul[i] = zero_tracking(intermediate_fuel_calcul[i])
+print(f"The copy is :\n{intermediate_fuel_calcul}\n")
 
 # print(f"The final result is {calculation_loop(sum_of_fuel)}")
 ## I do a copy because I need to use some mathematical elements
