187150a68a5cde1e20ba919a4475c13b9a4f3a32
Gautier ASTOUL <gastoul@aldebaran.lan>
2020-02-04 11:04:04 +0100

    new version not OK but close

--- Day1/1-2/Day1-2.py
+++ Day1/1-2/Day1-2.py
@@ -18,12 +18,18 @@ def sum_of_element(list):
 	# for parameter in range (len(list)-1):
 	# return list[parameter]
 
+# def zero_tracking(list_element, iteration):
 def zero_tracking(list_element):
-	list_element = calcul(list_element)
-	while list_element > 0 :
-		list_element = calcul(list_element)
-		result += list_element
-		return result
+    list_element = simple_calcul(list_element)
+    result = list_element
+    # z = 1
+    # print(f"test{iteration} / iteration n°{z} / calculation number {list_element} / result is {result}\n")
+    while simple_calcul(list_element) > 0 :
+        # z += 1
+        list_element = simple_calcul(list_element)
+        result += list_element
+        # print(f"test{iteration} / iteration n°{z} / calculation number {list_element} / result is {result}\n")
+    return result
 
 ## I open the file which containt some integers in strings
 ## I put them on a list, I transform them to be ready for calculation
@@ -39,12 +45,17 @@ sum_of_fuel = [simple_calcul(x) for x in sum_of_fuel]
 result_sum_of_fuel = sum_of_element(sum_of_fuel)
 print(f"The total of each element of fuel is:\n{result_sum_of_fuel}\n")
 
-##
+## 
 intermediate_fuel_calcul = sum_of_fuel.copy()
 print(f"The copy is :\n{intermediate_fuel_calcul}\n")
 for i in range (len(intermediate_fuel_calcul)):
-	intermediate_fuel_calcul[i] = zero_tracking(intermediate_fuel_calcul[i])
-print(f"The copy is :\n{intermediate_fuel_calcul}\n")
+# for i in range (2):
+	# intermediate_fuel_calcul[i] = zero_tracking(intermediate_fuel_calcul[i], i)
+    intermediate_fuel_calcul[i] = zero_tracking(intermediate_fuel_calcul[i])
+print(f"The additional fuel which is needed for our rocket is in this table :\n{intermediate_fuel_calcul}\n")
+final_sum_of_fuel = sum_of_element(intermediate_fuel_calcul) + result_sum_of_fuel
+print(f"\n{sum_of_element(intermediate_fuel_calcul)}\n")
+print(f"\n\nThe final result is :\n\n{final_sum_of_fuel}")
 
 # print(f"The final result is {calculation_loop(sum_of_fuel)}")
 ## I do a copy because I need to use some mathematical elements
